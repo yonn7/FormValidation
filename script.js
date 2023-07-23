59d76c6f9513e29a342b35bdffe25cec75c52683
Yonaskw <90166490+Yonaskw@users.noreply.github.com>
2023-07-22 20:57:54 -0700

    Update script.js
    created multiple functions for validations

--- script.js
+++ script.js
@@ -1,70 +1,94 @@
 const form = document.getElementById('form');
-const names = document.getElementById('name');
-const email = document.getElementById('email');
-const password = document.getElementById('password');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  const names1 = document.getElementById('name').value;
-  const email1 = document.getElementById('email').value;
-  const password1 = document.getElementById('password').value;
+  const namesValue = document.getElementById('name').value.trim();
+  const emailValue = document.getElementById('email').value.trim();
+  const passwordValue = document.getElementById('password').value.trim();
 
   // validateInputs();
   if (validateInputs()) {
     alert(
-      `Name: ${names1} ${'\n'}Email: ${email1} ${'\n'}Password: ${password1}`
+      `Name: ${namesValue} ${'\n'}Email: ${emailValue} ${'\n'}Password: ${passwordValue}`
     );
     form.submit();
   }
 });
 
-const validateInputs = () => {
-  const nameValue = names.value.trim();
-  const emailValue = email.value.trim();
-  const passwordValue = password.value.trim();
+const setErrormessage = (fieldName, errorMessage) => {
+  document.querySelector(fieldName).innerText = errorMessage;
+};
+
+const clearErrorMessage = (errorName) => {
+  document.querySelector(errorName).innerText = '';
+};
+
+const validateNameField = () => {
+  const namesValue = document.getElementById('name').value.trim();
+
+  if (namesValue === '') {
+    setErrormessage('.nameError', 'Name is Required');
+    result = false;
+  } else {
+    clearErrorMessage('.nameError');
+    result = true;
+  }
+  return result;
+};
+
+const validateEmail = () => {
+  const emailValue = document.getElementById('email').value.trim();
+
   const re =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  let result = false;
-
-  if (nameValue === '') {
-    document.querySelector('.nameError').innerText = 'Name is required';
-    result = false;
-  } else {
-    document.querySelector('.nameError').innerText = '';
-    result = true;
-  }
 
   if (emailValue === '') {
-    document.querySelector('.emailError').innerText = 'Email is required';
+    setErrormessage('.emailError', 'Email is Required');
     result = false;
   } else if (!re.test(String(emailValue).toLowerCase())) {
-    document.querySelector('.emailError').innerText =
-      'Please provide a proper email';
+    setErrormessage('.emailError', 'Please provide a proper email');
     result = false;
   } else {
-    document.querySelector('.emailError').innerText = '';
+    clearErrorMessage('.emailError');
     result = true;
   }
+  return result;
+};
+
+const validatePassword = () => {
+  const passwordValue = document.getElementById('password').value.trim();
 
   if (passwordValue === '') {
-    document.querySelector('.passwordError').innerText = 'Password is required';
+    setErrormessage('.passwordError', 'Password is Required');
     result = false;
   } else if (passwordValue.length < 5) {
-    document.querySelector('.passwordError').innerText =
-      'Password must be at least 5 character.';
+    setErrormessage('.passwordError', 'Password Must be at least 5 characters');
     result = false;
   } else {
-    document.querySelector('.passwordError').innerText = '';
+    clearErrorMessage('.passwordError');
     result = true;
   }
-
-  if (!this.form.checkbox.checked) {
-    document.querySelector('.checkError').innerText =
-      'Need to agree to Terms & Privacy';
-    return false;
-  } else {
-    document.querySelector('.checkError').innerText = '';
-  }
-
   return result;
+};
+
+const validateCheckbox = () => {
+  if (!this.form.checkbox.checked) {
+    setErrormessage('.checkError', 'Need to agree to Terms & Privacy');
+    result = false;
+  } else {
+    clearErrorMessage('.checkError');
+    result = true;
+  }
+  return result;
+};
+
+const validateInputs = () => {
+  let result = false;
+
+  const isNameValid = validateNameField();
+  const isEmailValid = validatePassword();
+  const isPasswordValid = validateEmail();
+  const isCheckedbox = validateCheckbox();
+
+  return isNameValid && isEmailValid && isPasswordValid && isCheckedbox;
 };
